e636f5c729974cad13fa8bf6fd7bfe6efc673868
projet <drikou29@gmail.com>
2018-06-01 16:12:53 +0200

    creation include head

---
+++ include/head.php
@@ -0,0 +1,12 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" type="image/png" href="../image/logo_burger_quizz.png"/>
+  <title>Burger Quiz</title>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
